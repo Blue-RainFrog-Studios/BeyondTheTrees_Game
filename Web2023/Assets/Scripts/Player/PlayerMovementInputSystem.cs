bcd30a4d3b7734965bb9da4cc086d0068971fc80
Daniel Capilla Sánchez <capilloski@gmail.com>
2023-10-14 17:28:36 +0200

    TouchControlsV1

---
+++ Web2023/Assets/Scripts/Player/PlayerMovementInputSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementInputSystem : MonoBehaviour
+{
+    private Rigidbody2D player_rb;
+    [SerializeField] private float speed = 8.0f;
+    [SerializeField] private float smoothness = 0.3f;
+
+    private Map playerInputActions;
+    private Vector2 direction;
+    private Vector2 velocity;
+    private void Awake()
+    {
+        player_rb = GetComponent<Rigidbody2D>();
+        playerInputActions = new Map();
+        playerInputActions.Enable();
+
+        //playerInputActions.Player.Move.performed += Move;
+    }
+    private void Update()
+    {
+        //Actualiza las posiciones que le decimos mediante el input
+        //Move
+        direction = playerInputActions.Player.Move.ReadValue<Vector2>();
+    }
+    private void FixedUpdate()
+    {
+        //Pintamos el movimiento del personaje con la interpolacion para que sea
+        //mas visual
+        player_rb.velocity = Vector2.SmoothDamp(player_rb.velocity, direction * speed, ref velocity, smoothness);
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log("Me estoy moviendo en: " + context.phase);
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        player_rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode2D.Force);
+        
+    }
+}
