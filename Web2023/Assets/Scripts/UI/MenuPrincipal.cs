60082889a64dc55e1a884b59591a03c9db31500a
Daniel Capilla Sánchez <capilloski@gmail.com>
2023-10-17 20:41:14 +0200

    Revert "Merge branch 'DaniCopia' into GeneracionEscenario5"
    This reverts commit a5307a2b4fc174e02a09debd08e06cd58e322551, reversing
    changes made to cb30867b9cc62b1a9d8d5f87b7b4d89c159fdd1d.

--- Web2023/Assets/Scripts/UI/MenuPrincipal.cs
+++ Web2023/Assets/Scripts/UI/MenuPrincipal.cs
@@ -5,11 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
-    public void jugar()
+    public void Jugar()
     {
-        //No usamos la escena
-        //Usamos los cambios de escena activando y desactivando (mas rapido)
-        SceneManager.LoadScene("LoginScene");
         Debug.Log("Entrando a la siguiente Pantalla");
     }
 }
