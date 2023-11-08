55a2061f22d979226f26165f4c971fa6c980c5a5
Daniel Capilla Sánchez <capilloski@gmail.com>
2023-11-08 19:29:15 +0100

    InvMovilFunciona

--- WEB2023/Assets/InventoryBtnScript.cs
+++ WEB2023/Assets/InventoryBtnScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class InventoryBtnScript : MonoBehaviour
 {
     public GameObject inventory;
-    // Start is called before the first frame update
+
     void Start()
     {
         this.gameObject.GetComponent<Button>().onClick.AddListener(() => { if (inventory.activeInHierarchy) { inventory.SetActive(false); } else { inventory.SetActive(true); } });
