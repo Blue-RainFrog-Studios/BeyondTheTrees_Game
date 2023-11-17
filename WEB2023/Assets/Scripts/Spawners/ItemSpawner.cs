73e768119488a0b7ea33bb39289de48f67af1abb
MrLuis33 <72052667+MrLuis33@users.noreply.github.com>
2023-11-17 19:55:35 +0100

    Spawner de Items Completado
    Se ha completado el script que define el comportamiento de este prefab.
    Tiene dos modos de spawnear objetos, puede hacer usando probabilidad simple o probabilidad baasada en la rareza de los items, se tiene que comentar o descomentar la linea en su script para cambiar su funcionalidad.

--- WEB2023/Assets/Scripts/Spawners/ItemSpawner.cs
+++ WEB2023/Assets/Scripts/Spawners/ItemSpawner.cs
@@ -1,15 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
     [SerializeField] All_Items_Database ItemDB;
+    // NO DEBEN SUMAR MÁS DE 100
+    [SerializeField] int CommonChance;
+    [SerializeField] int RareChance;
+    [SerializeField] int EpicChance;
+    [SerializeField] int LegendaryChance;
+
     public GameObject myItemPrefab;
 
     void Start()
     {
-        RandomItemFromRun();
+        //RandomItemFromRun();
+        RandomItem_BasedOnProbability_FromRun();
     }
 
     // Update is called once per frame
@@ -20,7 +29,11 @@ public class ItemSpawner : MonoBehaviour
 
     public void RandomItemFromRun()
     {
-        int RandomNumer = Random.Range(0, (ItemDB.Run_Items_Count + 1));
+        //int maxList = ItemDB.Run_Items_Count;
+        //int RandomNumer = Random.Range(0, (ItemDB.Run_Items_Count + 1));
+        //int RandomNumer = Random.Range(0, (maxList));
+        int RandomNumer = UnityEngine.Random.Range(0, ItemDB.Run_Items_Count);
+        //Debug.Log("Tamano pool" + maxList);
         ItemSO data = ItemDB.RunGetItemFromPool(RandomNumer);
 
         GameObject newItem = Instantiate(myItemPrefab, transform.position, Quaternion.identity);
@@ -30,5 +43,119 @@ public class ItemSpawner : MonoBehaviour
         itemComponent.InventoryItem = data;
         itemComponent.Quantity = 1;
 
+    }
+
+    public void RandomItem_BasedOnProbability_FromRun()
+    {
+        int rarityRandomizer = UnityEngine.Random.Range(0, 101);
+        string rarity = "Common";
+        if (rarityRandomizer <= CommonChance)
+        {
+            rarity = "Common";
+        }
+        if(rarityRandomizer > CommonChance && rarityRandomizer<= CommonChance+RareChance)
+        {
+            rarity = "Rare";
+        }
+        if (rarityRandomizer > CommonChance + RareChance && rarityRandomizer <= CommonChance + RareChance+EpicChance)
+        {
+            rarity = "Epic";
+        }
+        if (rarityRandomizer > CommonChance + RareChance + EpicChance && rarityRandomizer <= CommonChance + RareChance + EpicChance+ LegendaryChance)
+        {
+            rarity = "Legendary";
+        }
+        
+            switch (rarity)
+            {
+                case "Legendary":
+                // Si la lista de legendarios está vacía buscamos en los objetos epicos
+                if (!ItemDB.Run_Legendary_Items.Any())
+                {
+                    goto case "Epic";
+                }
+                else
+                {
+                    int RandomNumer = UnityEngine.Random.Range(0, ItemDB.Run_Legendary_Items.Count);
+                    //Debug.Log("Tamano pool" + maxList);
+                    ItemSO data = ItemDB.RunGetLegendaryItemFromPool(RandomNumer);
+
+                    GameObject newItem = Instantiate(myItemPrefab, transform.position, Quaternion.identity);
+
+                    Item itemComponent = newItem.GetComponent<Item>();
+
+                    itemComponent.InventoryItem = data;
+                    itemComponent.Quantity = 1;
+                }
+                    Debug.Log("Spawnea Lenegndary");
+                    break;
+                case "Epic":
+                // Si la lista de epicos está vacía buscamos en los objetos raros
+                if (!ItemDB.Run_Epic_Items.Any())
+                {
+                    goto case "Epic";
+                }
+                else
+                {
+                    int RandomNumer = UnityEngine.Random.Range(0, ItemDB.Run_Epic_Items.Count);
+                    //Debug.Log("Tamano pool" + maxList);
+                    ItemSO data = ItemDB.RunGetEpicItemFromPool(RandomNumer);
+
+                    GameObject newItem = Instantiate(myItemPrefab, transform.position, Quaternion.identity);
+
+                    Item itemComponent = newItem.GetComponent<Item>();
+
+                    itemComponent.InventoryItem = data;
+                    itemComponent.Quantity = 1;
+                }
+                Debug.Log("Spawnea Epic");
+                    break;
+                case "Rare":
+                // Si la lista de raros está vacía buscamos en los objetos comunes
+                if (!ItemDB.Run_Rare_Items.Any())
+                {
+                    goto case "Common";
+                }
+                else
+                {
+                    int RandomNumer = UnityEngine.Random.Range(0, ItemDB.Run_Rare_Items.Count);
+                    //Debug.Log("Tamano pool" + maxList);
+                    ItemSO data = ItemDB.RunGetRareItemFromPool(RandomNumer);
+
+                    GameObject newItem = Instantiate(myItemPrefab, transform.position, Quaternion.identity);
+
+                    Item itemComponent = newItem.GetComponent<Item>();
+
+                    itemComponent.InventoryItem = data;
+                    itemComponent.Quantity = 1;
+                }
+                Debug.Log("Spawnea Rare");
+                    break;
+                
+            case "Common":
+                if (!ItemDB.Run_Common_Items.Any())
+                {
+                    goto default;
+                }
+                else
+                {
+                    int RandomNumer = UnityEngine.Random.Range(0, ItemDB.Run_Common_Items.Count);
+                    //Debug.Log("Tamano pool" + maxList);
+                    ItemSO data = ItemDB.RunGetCommonItemFromPool(RandomNumer);
+
+                    GameObject newItem = Instantiate(myItemPrefab, transform.position, Quaternion.identity);
+
+                    Item itemComponent = newItem.GetComponent<Item>();
+
+                    itemComponent.InventoryItem = data;
+                    itemComponent.Quantity = 1;
+                }
+                Debug.Log("Spawnea Common");
+                    break;
+
+                default:
+                    Debug.Log("No se ha spawneado nada");
+                    break;
+            }
     }
 }
--- WEB2023/Assets/Scripts/UI/Tienda/All_Items_Database.cs
+++ WEB2023/Assets/Scripts/UI/Tienda/All_Items_Database.cs
@@ -10,6 +10,10 @@ public class All_Items_Database : ScriptableObject
     public List<ItemSO> All_items;
     public List<ItemSO> Shop_Items;
     public List<ItemSO> Run_Items;
+    public List<ItemSO> Run_Common_Items;
+    public List<ItemSO> Run_Rare_Items;
+    public List<ItemSO> Run_Epic_Items;
+    public List<ItemSO> Run_Legendary_Items;
 
     public int All_Items_Count
     {
@@ -32,6 +36,22 @@ public class All_Items_Database : ScriptableObject
     {
         Shop_Items[index].IsPurchased = true;
         Run_Items.Add(Shop_Items[index]);
+        int itemRarity = Shop_Items[index].Rarity;
+        switch (itemRarity)
+        {
+            case 0:
+                Run_Common_Items.Add(Shop_Items[index]);
+                break;
+            case 1:
+                Run_Rare_Items.Add(Shop_Items[index]);
+                break;
+            case 2:
+                Run_Epic_Items.Add(Shop_Items[index]);
+                break;
+            case 3:
+                Run_Legendary_Items.Add(Shop_Items[index]);
+                break;
+        }
     }
 
     public ItemSO ShopGetItemFromPool(int index)
@@ -41,5 +61,25 @@ public class All_Items_Database : ScriptableObject
     public ItemSO RunGetItemFromPool(int index)
     {
         return Run_Items[index];
+    }
+    public ItemSO RunGetCommonItemFromPool(int index)
+    {
+        return Run_Common_Items[index];
+    }
+    public ItemSO RunGetRareItemFromPool(int index)
+    {
+        return Run_Rare_Items[index];
+    }
+    public ItemSO RunGetEpicItemFromPool(int index)
+    {
+        return Run_Epic_Items[index];
+    }
+    public ItemSO RunGetLegendaryItemFromPool(int index)
+    {
+        return Run_Legendary_Items[index];
+    }
+    public void RelocateItemsRarity()
+    {
+
     }
 }
