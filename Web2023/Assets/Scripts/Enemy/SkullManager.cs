2cdfa629afb7cc5582f4eb66b3ab4305091ab0af
Daniel Capilla Sánchez <capilloski@gmail.com>
2023-11-17 13:02:02 +0100

    MissingReference arreglado

--- Web2023/Assets/Scripts/Enemy/SkullManager.cs
+++ Web2023/Assets/Scripts/Enemy/SkullManager.cs
@@ -13,25 +13,27 @@ public class SkullManager : MonoBehaviour
     {
         if(collision.CompareTag("Enemy"))
         {
-            Debug.Log("PENE");
             if(collision.gameObject.GetComponent<EnemyController>().life <= 0)
             {
-                Debug.Log("TETAS GORDAS");
-                if(skull.GetComponent<Skull>().valor == 9)
+                if(skull != null) 
                 {
-                    skull.GetComponent<Animator>().SetTrigger("Fase 1-2");
-                    skull.GetComponent<Skull>().valor = 6;
+                    if (skull.GetComponent<Skull>().valor == 9)
+                    {
+                        skull.GetComponent<Animator>().SetTrigger("Fase 1-2");
+                        skull.GetComponent<Skull>().valor = 6;
+                    }
+                    else if (skull.GetComponent<Skull>().valor == 6)
+                    {
+                        skull.GetComponent<Animator>().SetTrigger("Fase 2-3");
+                        skull.GetComponent<Skull>().valor = 3;
+                    }
+                    else if (skull.GetComponent<Skull>().valor == 3)
+                    {
+                        skull.GetComponent<Animator>().SetTrigger("Fase 3-4");
+                        skull.GetComponent<Skull>().valor = 0;
+                    }
                 }
-                else if (skull.GetComponent<Skull>().valor == 6)
-                {
-                    skull.GetComponent<Animator>().SetTrigger("Fase 2-3");
-                    skull.GetComponent<Skull>().valor = 3;
-                }
-                else if (skull.GetComponent<Skull>().valor == 3)
-                {
-                    skull.GetComponent<Animator>().SetTrigger("Fase 3-4");
-                    skull.GetComponent<Skull>().valor = 0;
-                }
+                
 
             }
         }
