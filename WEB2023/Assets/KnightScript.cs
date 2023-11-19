c8668d1cbf50ebfa5e3018786e0566da34a59c55
DanielGarate <dgaratediaz@gmail.com>
2023-11-19 14:53:28 +0100

    Interfaces
    Se ha hecho que las interfaces iniciales tengan la misma estética

--- WEB2023/Assets/KnightScript.cs
+++ WEB2023/Assets/KnightScript.cs
@@ -31,7 +31,7 @@ public class KnightScript : MonoBehaviour
     public KnightScript() 
     {
         totalHealth = 50;
-        health = 11111150;
+        health = 50;
         speed = 6;
         attack = 30;
         defense = 7;
@@ -51,7 +51,7 @@ public class KnightScript : MonoBehaviour
         if (knight.health <= 0)
         {
             SceneManager.LoadScene("GameOver");
-            GetComponent<PlayerMovementInputSystem>().nivel =0;
+            GetComponent<PlayerMovementInputSystem>().nivel = 0;
             knight.health = 50;
             lifeBar.value = knight.health;
             this.gameObject.transform.position = new Vector2(0, -4);
