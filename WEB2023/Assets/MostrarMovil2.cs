1a8721a2bbfe12e32c96ce68c076aa9051dcc94b
Daniel Capilla Sánchez <capilloski@gmail.com>
2023-12-01 00:19:27 +0100

    sda

--- WEB2023/Assets/MostrarMovil2.cs
+++ WEB2023/Assets/MostrarMovil2.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MostrarMovil2 : MonoBehaviour
 {
+    private Vector3 posicion;
+    
     private void Start()
     {
-        //gameObject.SetActive(Application.isMobilePlatform);
+        posicion = transform.position;
     }
     private void Update()
     {
-        if(Input.touchCount > 0)
+        if (Input.touchCount > 0 )
         {
-            Debug.Log("Puton");
-            //Input.GetAxis
-            //gameObject.transform.position = 
-            //gameObject.transform.localPosition = Input.mousePosition;
-            Touch touch = Input.GetTouch(0);
-            gameObject.transform.position = touch.position;//Input.mousePosition;
+            //if(!GetComponent<Image>().enabled)
+                GetComponent<Image>().enabled = true;
+            //gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Touch touch = Input.GetTouch(0);
+                gameObject.transform.position = touch.position;
+                posicion = touch.position;
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                //gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                //if (GetComponent<Image>().enabled)
+                GetComponent<Image>().enabled = false;
+            }
+
+
         }
+       
     }
-   
+
+
 }
