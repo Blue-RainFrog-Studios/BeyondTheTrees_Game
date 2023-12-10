cb67b555d55247df1c329bb5fc5175c553bca7c5
MrLuis33 <72052667+MrLuis33@users.noreply.github.com>
2023-12-10 07:41:50 +0100

    Lobo Funcionando
    La FMS del lobo ya funciona, hay que retocar las animaciones para que sean más fluidas

--- WEB2023/Assets/Prefab/Enemies/Wolf/FSMWolf.cs
+++ WEB2023/Assets/Prefab/Enemies/Wolf/FSMWolf.cs
@@ -6,7 +6,7 @@ using BehaviourAPI.Core.Actions;
 using BehaviourAPI.StateMachines;
 using BehaviourAPI.Core.Perceptions;
 using BehaviourAPI.UnityToolkit;
-
+using BehaviourAPI.StateMachines.StackFSMs;
 
 
 public class FSMWolf : BehaviourRunner
@@ -15,6 +15,7 @@ public class FSMWolf : BehaviourRunner
     Actions_Wolf _ActionsWolf;
     public PushPerception itemExist; //mirar distanca a enemigo
     public PushPerception iCrashed; //mirar distanca a enemigo
+    public PushPerception iLoseItem; // Comprueba si el item se ha perdido
     protected override void Init()
     {
         _ActionsWolf = GetComponent<Actions_Wolf>();
@@ -22,6 +23,7 @@ public class FSMWolf : BehaviourRunner
     }
     protected override BehaviourGraph CreateGraph()
     {
+        //StackFSM WolfFsm = new StackFSM();
         FSM WolfFsm = new FSM();
 
         #region Actions
@@ -37,6 +39,10 @@ public class FSMWolf : BehaviourRunner
 
         FunctionalAction DazeToPlayerAction = new FunctionalAction(_ActionsWolf.StartMethodWalk, _ActionsWolf.UpdateMethodWalk);
 
+        FunctionalAction WalkToItemAction = new FunctionalAction(_ActionsWolf.StartMethodItemChase, _ActionsWolf.UpdateMethodItemChase);
+
+        FunctionalAction DestroyItem = new FunctionalAction(_ActionsWolf.StartMethodDestroyItem, _ActionsWolf.UpdateMethodDestroyItem);
+
 
         //------------------ cambio de maquina
         //FunctionalAction TiredAction = new FunctionalAction(_ActionsWolf.StartMethodTired, _ActionsWolf.UpdateMethodTired);
@@ -47,6 +53,10 @@ public class FSMWolf : BehaviourRunner
         State Wating = WolfFsm.CreateState(WatingAction); //SOLO REPETIR 1 vez
 
         State WalkToPlayer = WolfFsm.CreateState(WalkToPlayerAction); //el estado WalkToPlayer se crea con la acción WalkToPlayerAction
+
+        State WalkToItem = WolfFsm.CreateState(WalkToItemAction);
+
+        State DestroyingItem = WolfFsm.CreateState(DestroyItem);
         
         State Charging = WolfFsm.CreateState(ChargingToPlayerAction); 
 
@@ -68,9 +78,14 @@ public class FSMWolf : BehaviourRunner
         /**/
         ConditionPerception noCollisionWithYAxis = new ConditionPerception(() => !_ActionsWolf.CheckCollisionWithYAxis());
 
-        //ConditionPerception HPLow = new ConditionPerception(() => _ActionsDavidElGnomo.CheckHPLow());
+        ConditionPerception HPLow = new ConditionPerception(() => _ActionsWolf.CheckHPLow());
 
-        ConditionPerception itmeExist = new ConditionPerception(() => _ActionsWolf.CheckItemExist());
+        ConditionPerception itemExist = new ConditionPerception(() => _ActionsWolf.CheckItemExist());
+
+        ConditionPerception itemPickedByPlayer = new ConditionPerception(() => !_ActionsWolf.CheckItemExist());
+
+        ConditionPerception itemDestroyed = new ConditionPerception(() => _ActionsWolf.CheckItemExist());
+
 
         //
         #endregion
@@ -88,6 +103,11 @@ public class FSMWolf : BehaviourRunner
 
         StateTransition Bite_to_WalkToPlayer = WolfFsm.CreateTransition(Bite, WalkToPlayer, statusFlags: StatusFlags.Finished); // new wolf 
 
+        StateTransition WalkingToPlayer_to_WalkingItem = WolfFsm.CreateTransition(WalkToPlayer, WalkToItem, itemExist);
+
+        StateTransition WalkingItem_to_DestroyingItem = WolfFsm.CreateTransition(WalkToItem, DestroyingItem, statusFlags: StatusFlags.Success);
+                                                                                                                   //De momento solo se tiene en cuenta que consigue comer                                 
+        StateTransition DestroyingItem_to_WalkingToPlayer = WolfFsm.CreateTransition(DestroyingItem, WalkToPlayer, statusFlags: StatusFlags.Success);
 
 
         //push perceptions
@@ -96,10 +116,14 @@ public class FSMWolf : BehaviourRunner
         StateTransition Bite_to_Dazed = WolfFsm.CreateTransition(Bite, Dazed, statusFlags: StatusFlags.Success); // new wolf dazed
         //StateTransition WalkingToPlayerAttack_to_Tired = WolfFsm.CreateTransition(WalkAttack, Tired, statusFlags: StatusFlags.Success);
         StateTransition Dazed_to_WalkingToPlayer = WolfFsm.CreateTransition(Dazed, WalkToPlayer, statusFlags: StatusFlags.Finished);
+
+
+        StateTransition WalkingItem_to_WalkToPlayer = WolfFsm.CreateTransition(WalkToItem, WalkToPlayer, statusFlags: StatusFlags.Finished);
         #endregion
 
         #region Push Perceptions
         iCrashed = new PushPerception(Bite_to_Dazed);
+        iLoseItem = new PushPerception(WalkingItem_to_WalkToPlayer);
         //itemExist = new PushPerception(WalkToPlayer_to_WalkToItem, Waitintg_to_WalkToItem, Bite_to_WalkToItem, Dazed_to_WalkToItem); //para cuando tenga el imtem
         #endregion
 
@@ -109,5 +133,10 @@ public class FSMWolf : BehaviourRunner
     public void Crash()
     {
         iCrashed.Fire();
+    }
+
+    public void LoseItem()
+    {
+        iLoseItem.Fire();
     }
 }
